7849c2b6666778f803cde2a2ce24629b5b97bbde
ValentinoT <danieltorresc13@gmail.com>
2020-05-30 16:40:46 -0500

    Inicio Growlii

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+const Index = () => {
+    return(
+        <h1>Hola Mundo</h1>
+    )
+}
+
+export default Index
---
+++ pages/registro.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+const Title = styled.h1`
+    color: red;
+`
+
+const Registro = () => {
+    return(
+        <Title>Registro</Title>
+    )
+}
+
+export default Registro
